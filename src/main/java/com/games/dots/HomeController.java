1668823364a1b601ba67ad89ebd5a36527de21e2
Marat Asadurian <ikar2000@gmail.com>
2014-01-01 23:59:00 +0200

    Moves controller

--- src/main/java/com/games/dots/HomeController.java
+++ src/main/java/com/games/dots/HomeController.java
@@ -1,7 +1,5 @@
 package com.games.dots;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
@@ -26,12 +24,6 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
 	}
---
+++ src/main/java/com/games/dots/enities/Coordinates.java
@@ -0,0 +1,12 @@
+package com.games.dots.enities;
+
+import java.io.Serializable;
+
+public class Coordinates implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public int x;
+	public int y;
+}
