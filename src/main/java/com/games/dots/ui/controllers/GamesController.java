543a3fc12b5a9580c7d1e01dd5a1dd7455c55cf4
Asadurian <asaduria@685b35c9c89a.ant.amazon.com>
2014-09-28 21:33:19 -0700

    First commit after long time

--- src/main/java/com/games/dots/ui/controllers/GamesController.java
+++ src/main/java/com/games/dots/ui/controllers/GamesController.java
@@ -93,7 +93,7 @@ public class GamesController {
 	    HttpHeaders headers = new HttpHeaders();
 	    headers.setLocation(uriComponents.toUri());
 	    com.games.dots.ui.entities.Game uiGame = new com.games.dots.ui.entities.Game(game);
-	    return new ResponseEntity<com.games.dots.ui.entities.Game>(uiGame, headers, HttpStatus.OK);	    
+	    return new ResponseEntity<>(uiGame, headers, HttpStatus.OK);
 	}
 	
 	@RequestMapping(value = "/games/{gameId}/players", method = RequestMethod.POST)
